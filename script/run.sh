79014892620dd4a60c91ff477fc79f9c0dabfcde
bakuppus <bakuppus@gmail.com>
2019-04-07 20:05:18 +0000

    sec

--- script/run.sh
+++ script/run.sh
@@ -1,1 +1,4 @@
 touch /tmp/myfile
+vault secrets tune -max-lease-ttl=87600h pki
+vault write -field=certificate pki/root/generate/internal  common_name="example.com" ttl=87600h > CA_cert.crt
+vault write pki/config/urls issuing_certificates="http://127.0.0.1:8200/v1/pki/ca" crl_distribution_points="http://127.0.0.1:8200/v1/pki/crl"
